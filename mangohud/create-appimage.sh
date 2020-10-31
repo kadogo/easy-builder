3083fa7486a387149ae58a44a8c37b9b2aa4a528
kadogo <kadogo@users.noreply.github.com>
2020-10-31 19:24:07 +0100

    Use symlink and mechanism to activate MangoHud for vulkan for AppImage

--- mangohud/create-appimage.sh
+++ mangohud/create-appimage.sh
@@ -22,53 +22,79 @@ EOF
 # Create wrapper
 # Carefull to escape $ and backslash properly
 # https://github.com/probonopd/linuxdeployqt/wiki/Custom-wrapper-script-instead-of-AppRun
-cat << EOF > ./AppDir/AppRun
+cat <<'EOF' > ./AppDir/AppRun
 #!/bin/bash
 
-if [ "\$#" -eq 0 ]; then
-	programname=`basename "$0"`
-	echo "ERROR: No program supplied (in full path)"
-	echo
-	echo "Usage: $programname <program>"
-	exit 1
+HERE="$(dirname "$(readlink -f "${0}")")"
+
+if [ "$#" -eq 0 ]; then
+        programname=`basename "$0"`
+        echo "ERROR: No program supplied"
+        echo
+        echo "Usage: $programname <program>"
+        exit 1
 fi
 
-HERE="\$(dirname "\$(readlink -f "\${0}")")"
-
-if [ "\$@" = "--dlsym" ]; then
+if [ "$1" = "--dlsym" ]; then
         MANGOHUD_DLSYM=1
         shift
 fi
 
 MANGOHUD_LIB_NAME="libMangoHud.so"
 
-if [ "\$MANGOHUD_DLSYM" = "1" ]; then
-        MANGOHUD_LIB_NAME="libMangoHud_dlsym.so:\${MANGOHUD_LIB_NAME}"
+if [ "$MANGOHUD_DLSYM" = "1" ]; then
+        MANGOHUD_LIB_NAME="libMangoHud_dlsym.so:${MANGOHUD_LIB_NAME}"
 fi
 
-LD_PRELOAD="\${LD_PRELOAD}:\${MANGOHUD_LIB_NAME}"
+# Preload using the plain filesnames of the libs, the dynamic linker will
+# figure out whether the 32 or 64 bit version should be used, and will search
+# for it in the correct directory
+LD_PRELOAD="${LD_PRELOAD}:${MANGOHUD_LIB_NAME}"
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$HERE/usr/lib/mangohud/\$LIB/"
 
-# Define which lib to load with MANGOHUDARCH
-if [[ "\$MANGOHUDARCH" =  'x86-64' ]] ; then
-  LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:\${HERE}/usr/lib/mangohud/lib/" 
-else
-  LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:\${HERE}/usr/lib/mangohud/lib32/"
-fi
-
-cd "${HERE}/usr"
+# Add MangoHud implicit layer to VK_LAYER_PATH and make a version where $HERE is in library_path and that we want activate the MangoHud layer
+VK_LAYER_PATH="${VK_LAYER_PATH}:$HERE/usr/share/vulkan/implicit_layer.d/"
+VK_INSTANCE_LAYERS=${VK_INSTANCE_LAYERS}:VK_LAYER_MANGOHUD_overlay 
 
 # DEBUG
-echo "HERE: \$HERE"
-echo "MANGOHUDARCH: \$MANGOHUDARCH"
-echo "LD_PRELOAD: \$LD_PRELOAD"
-echo "LD_LIBRARY_PATH: \$LD_LIBRARY_PATH"
-echo "\\\$@: \$@"
+echo "HERE: $HERE"
+echo "LD_PRELOAD: $LD_PRELOAD"
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+echo "VK_LAYER_PATH: $VK_LAYER_PATH"
+echo "VK_INSTANCE_LAYERS: $VK_INSTANCE_LAYERS"
+echo "\$@: $@"
 
-exec env MANGOHUD=1 LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}" LD_PRELOAD="\${LD_PRELOAD}" "\$@"
+exec env \
+  MANGOHUD=1 \
+  LD_LIBRARY_PATH="${LD_LIBRARY_PATH}" \
+  LD_PRELOAD="${LD_PRELOAD}" \
+  VK_LAYER_PATH="${VK_LAYER_PATH}" \
+  VK_INSTANCE_LAYERS=${VK_INSTANCE_LAYERS} \
+  "$@"
 EOF
 
 # Set execute bit
 chmod +x ./AppDir/AppRun
+
+# Create symlink in AppDir like the mangohud-setup script
+cd ./AppDir/usr/
+# FIXME get the triplet somehow
+ln -sv lib ././lib/mangohud/lib64
+ln -sv lib ././lib/mangohud/x86_64
+ln -sv lib ././lib/mangohud/x86_64-linux-gnu
+ln -sv . ././lib/mangohud/lib/x86_64
+ln -sv . ././lib/mangohud/lib/x86_64-linux-gnu
+ln -sv lib32 ././lib/mangohud/i686
+ln -sv lib32 ././lib/mangohud/i386-linux-gnu
+ln -sv ../lib32 ././lib/mangohud/lib/i386-linux-gnu
+ln -sv lib32 ././lib/mangohud/i686-linux-gnu
+ln -sv ../lib32 ././lib/mangohud/lib/i686-linux-gnu
+#ln -sv lib /usr/lib/mangohud/aarch64-linux-gnu
+#ln -sv lib /usr/lib/mangohud/arm-linux-gnueabihf
+cd ../../
+
+# Make the implicit vulkan layer with a relative library_path
+sed -i 's#/usr/#../../../#g' ./AppDir/usr/share/vulkan/implicit_layer.d/MangoHud.json
 
 # Build appimage with linuxdeploy
 # Need to define ARCH for no reason
